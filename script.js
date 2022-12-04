7eb343aca8418822a26fb7af57f1055bc7b7562e
Joe-Hsn <youssef.hassan.mahmoud@gmail.com>
2022-12-04 23:17:14 +0200

    restaurant form & showcase are done

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const toTop = document.querySelector("#totop");
 
-// AOS
-// AOS.init({
-//   duration: 800,
-//   easing: "ease-in-out",
-//   once: true,
-// });
-// to top button
 toTop.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
